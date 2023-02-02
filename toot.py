8f1f17e9698cd88254e02c790eac26e136f01541
YepesD <124261546+YepesD@users.noreply.github.com>
2023-02-02 12:42:20 +0100

    Add files via upload

---
+++ toot.py
@@ -0,0 +1,29 @@
+from mastodon import Mastodon
+from datetime import datetime
+from os import listdir
+import os
+from random import random
+from random import randint
+import time
+mastodon = Mastodon(
+    access_token = 'bc2TBkikIxF8JV7F6InYfn3FOv4jk6MMD3ggZE_AiB8',
+    api_base_url = 'https://owo.cafe'
+    )
+files = []
+dias = ["Es lunes", "Es martes", "Es miércoles", "Es jueves", "Es viernes", "Es sábado", "Es domingo"]
+dt = datetime.now()
+x = dt.weekday()
+
+path = "./dias/" + str(x) + "/"
+files = os.listdir(path)
+texto = dias[x]
+num_files = len(files)
+if num_files >= 1:
+    position = randint(0, num_files-1)
+    file = files[position]
+
+    media_path = path + file
+
+    media = mastodon.media_post(media_path)
+    time.sleep(10)
+    toot = mastodon.status_post(texto, media_ids=media)
