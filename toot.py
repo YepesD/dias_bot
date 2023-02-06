f057702d9e310039c4d0da3117433076d0042e4c
YepesD <124261546+YepesD@users.noreply.github.com>
2023-02-06 22:26:39 +0100

    Update toot.py

--- toot.py
+++ toot.py
@@ -1,17 +1,20 @@
 from mastodon import Mastodon
 from datetime import datetime
-from os import listdir
 import os
 from random import random
 from random import randint
 import time
+import xml.etree.ElementTree as ET
+
+tree = ET.parse('./dias/desc.xml')
+path = "./description"
+root = tree.getroot()
 
 mastodon = Mastodon(
     access_token = os.environ['KEY'],
     api_base_url = 'https://owo.cafe'
     )
 files = []
-dias = ["Es lunes", "Es martes", "Es miércoles", "Es jueves", "Es viernes!!!!", "Es sábado", "Es domingo"]
 dt = datetime.now()
 x = dt.weekday()
 
@@ -22,15 +25,10 @@ num_files = len(files)
 if num_files >= 1:
     position = randint(0, num_files-1)
     file = files[position]
-    with open(r'./dias/desc.txt', 'r') as fp:
-        lines = fp.readlines()
-        for line in lines:
-            if line.find(file) != -1:
-                    line_split = line.split(":")
-                    desc = line_split[2]
-                    texto = line_split[1]
-    if texto == "":
-        texto = dias[x]
+    path_id = path + "[@id='" + file + "']"
+    for item in root.findall(path_id):
+        texto = item[0].text
+        desc  = item[1].text
     media_path = path + file
     media = mastodon.media_post(media_path, description=desc)
     time.sleep(30)
