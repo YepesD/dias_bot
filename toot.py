80416f107af089bc8a9a39440b494a5ef0157d21
YepesD <124261546+YepesD@users.noreply.github.com>
2023-02-08 10:16:08 +0100

    Update toot.py

--- toot.py
+++ toot.py
@@ -5,30 +5,38 @@ from random import random
 from random import randint
 import time
 import xml.etree.ElementTree as ET
-
+#Abrimos XML con los datos de textos
 tree = ET.parse('./dias/desc.xml')
 xmlpath = "./description"
 root = tree.getroot()
-
+#Objeto de Mastodon
 mastodon = Mastodon(
     access_token = os.environ['KEY'],
     api_base_url = 'https://owo.cafe'
     )
+#Seleccionamos día de la semana
 files = []
 dt = datetime.now()
 x = dt.weekday()
-
+#Abrimos carpeta correspondiente al día de la semana
 path = "./dias/" + str(x) + "/"
 files = os.listdir(path)
 num_files = len(files)
+#Si encontramos archivos...
 if num_files >= 1:
+    #Seleccionamos uno al azar
     position = randint(0, num_files-1)
     file = files[position]
+    #Buscamos su información en el archivo de textos
     path_id = xmlpath + "[@id='" + file + "']"
     for item in root.findall(path_id):
-        texto = item[0].text
-        desc  = item[1].text
+        texto = item[0].text #Texto del toot
+        desc  = item[1].text #Alt text
+    #Concatenamos la ruta completa
     media_path = path + file
+    #Anexamos el archivo al objeto media_post de Mastodon
     media = mastodon.media_post(media_path, description=desc)
+    #Dejamos que se procese todo trankiliyo sosegado relajao relajao
     time.sleep(30)
+    #Tooteamos
     toot = mastodon.status_post(texto, media_ids=media)
